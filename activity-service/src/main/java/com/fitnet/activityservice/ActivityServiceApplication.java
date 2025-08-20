f331224a160d77b562a25bd06f8d1db7f9261b55
Dangtai <daodangtai2212@gmail.com>
2025-08-20 13:09:16 +0700

    creating eureka server

--- activity-service/src/main/java/com/fitnet/activityservice/ActivityServiceApplication.java
+++ activity-service/src/main/java/com/fitnet/activityservice/ActivityServiceApplication.java
@@ -2,8 +2,10 @@ package com.fitnet.activityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ActivityServiceApplication {
 
 	public static void main(String[] args) {
